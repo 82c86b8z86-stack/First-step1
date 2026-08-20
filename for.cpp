a4ec45d1c3c32e9da8f43ee61ed729b0bdc3abc8
Jeffrey_Vong <82c86b8z86@privaterelay.appleid.com>
2026-08-20 21:52:29 +0800

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>  
+
+int main()
+{
+int num=1;
+int sum=0;
+for(num;num<=100;num+=1)                                         //++ & +=1都差不多，在原有基础上加1
+{
+    sum=sum+num;
+}
+//%d表示将字符串后面的对应位置参数用这怎整数形式打印
+printf("Sum of numbers from 1 to 100 is: %d\n", sum);
+    return 0;
+}
