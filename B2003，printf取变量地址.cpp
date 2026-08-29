b21a87df123c6c667ecdf532a077c3c23455c8e9
Jeffrey_Vong <82c86b8z86@privaterelay.appleid.com>
2026-08-29 20:27:32 +0800

    Add files via upload

---
+++ B2003，printf取变量地址.cpp
@@ -0,0 +1,21 @@
+//#include<cstdio>
+//#include<iostream>
+//using namespace std;
+
+//int main()
+//{
+  //  int a,b,c;
+    //cin>>a>>b>>c;
+    //cout<<b<<endl;
+    //return 0;
+//}
+#include<cstdio>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n", b);
+    return 0;
+}
