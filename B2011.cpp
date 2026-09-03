0fe8ec2d3d00821501ffcc291757041ca21a72c7
Jeffrey_Vong <82c86b8z86@privaterelay.appleid.com>
2026-09-03 13:48:55 +0800

    Add files via upload

---
+++ B2011.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	double a,b,c;
+	cin>>a>>b;
+	c=a/b;
+	cout<<fixed<<setprecision(9)<<c<<endl;
+	return 0;
+}
