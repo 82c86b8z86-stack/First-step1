a70ebceba0675cce4272e318bb02c6907e75cc01
Jeffrey_Vong <82c86b8z86@privaterelay.appleid.com>
2026-09-16 18:20:21 +0800

    Add files via upload

---
+++ B2023.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	char a;
+	int b;
+	float c;
+	double d;
+	cin>>a>>b>>c>>d;
+	cout<<a<<" "<<b<<" "<<fixed<<setprecision(6)<<c<<" "<<d<<endl;
+	return 0;
+}
