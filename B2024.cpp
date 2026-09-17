4e2ae20c0854f4560cd94822902e36ff40eee1d7
Jeffrey_Vong <82c86b8z86@privaterelay.appleid.com>
2026-09-17 13:53:07 +0800

    Add files via upload

---
+++ B2024.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	double n;
+	scanf("%lf",&n);
+	printf("%.6f",n);
+	printf("\n%0.5f",n);
+	printf("\n%e",n);
+	printf("\n%.6g",n);
+	return 0;
+}
