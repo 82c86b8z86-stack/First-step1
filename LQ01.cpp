86346bcb93d17b06839d8266e5ac548e13256514
Jeffrey_Vong <82c86b8z86@privaterelay.appleid.com>
2026-09-24 23:21:15 +0800

    Add files via upload

--- LQ01.cpp
+++ LQ01.cpp
@@ -1,43 +1,11 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-    int n;
-    cin >> n;
-    int a[100005] = {0};     
-    for (int i = 0; i < n; i++) 
-	{
-        cin >> a[i];
-    }
-    sort(a, a + n);
-    
-    int b[100005] = {0};     
-    int cnt = 0;             
-    
-    int i = 0;
-    while (i < n) 
-	{
-        int j = i;
-        while (j < n && a[j] == a[i])
-		{ 
-            j++;
-        }
-        b[cnt] = j - i;     
-        cnt++;               
-        i = j;                
-    }
-    
-    int max = b[0];      
-    for (int i = 1; i < cnt; i++)
-	{
-        if (b[i] > max) 
-		{
-            max = b[i];
-        }
-    }
-    
-    cout << n - max << endl;
-    return 0;
+	int m,n,y;
+	cin>>m>>n;
+	y=m/n +1;
+	cout<<y<<endl;
+	return 0;
 }
